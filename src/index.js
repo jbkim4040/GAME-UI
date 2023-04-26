832040f1c5b9308726690a2f8d824f0ae6784180
jspeacj <jspeacj@naver.com>
2023-04-26 15:59:30 +0900

    MainPage_Template Apply

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './App';
-=======
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
->>>>>>> aa7ca7984c7bf4da67687f4612604980820b5da3
 
 ReactDOM.render(
   <React.StrictMode>
